88d60c449743ebe0cb6d85184458ad5a302d7ed4
JonathanMooMijares <l22390450@chetumal.tecnm.mx>
2026-05-28 07:25:13 -0500

    Agregación de módulo de vista y método nuevo

--- src/views/NuevoLibroView.vue
+++ src/views/NuevoLibroView.vue
@@ -1,9 +1,79 @@
 <script>
+import { ref } from 'vue';
+import axios from 'axios';
+export default {
+    setup() {
+        const nuevoLibro = ref({
+            titulo: '',
+            autor: '',
+            ISBN: '',
+            genero: '',
+            precio: '',
+            disponibilidad: ''
+
+        })
+
+        const crearLibro = async () => {
+            try {
+                const response = await axios.post('http://localhost:3000/libros', nuevoLibro.value);
+
+                setTimeout(() => {
+                   alert("Libro creado correctamente")
+                },200)
+                nuevoLibro.value.titulo = '',
+                nuevoLibro.value.autor = '',
+                nuevoLibro.value.ISBN = '',
+                nuevoLibro.value.genero = '',
+                nuevoLibro.value.precio = '',
+                nuevoLibro.value.disponibilidad = ''
+                console.log("Libro creado", response.data)
+            } catch (error) {
+                console.log("Error al crear el libro", error)
+            }
+        }
+
+        return {
+            nuevoLibro,
+            crearLibro
+        }
+    }
+}
 </script>
 
 <template>
-    <h1>Nuevo Libro</h1>
+    <main>
+        <form @submit.prevent="crearLibro">
+            <div>
+                <div>
+                    <label for="titulo"></label>
+                    <input name="titulo" v-mode="nuevoLibro.titulo" type="text" placeholder="Titulo">
+                </div>
+                <div>
+                    <label for="autor"></label>
+                    <input name="autor" v-mode="nuevoLibro.autor" type="text" placeholder="Autor">
+                </div>
+                <div>
+                    <label for="ISBN"></label>
+                    <input name="ISBN" v-mode="nuevoLibro.ISBN" type="text" placeholder="ISBN">
+                </div>
+                <div>
+                    <label for="genero"></label>
+                    <input name="genero" v-mode="nuevoLibro.genero" type="text" placeholder="Género">
+                </div>
+                <div>
+                    <label for="precio"></label>
+                    <input name="precio" v-mode="nuevoLibro.precio" type="text" placeholder="Precio">
+                </div>
+                <div>
+                    <label for="disponibilidad"></label>
+                    <input name="disponibilidad" v-mode="nuevoLibro.disponibilidad" type="text"
+                        placeholder="Disponibilidad">
+                </div>
+
+            </div>
+            <button type="submit">Crear nuevo libro</button>
+        </form>
+    </main>
 </template>
 
-<style>
-</style>
+<style></style>
